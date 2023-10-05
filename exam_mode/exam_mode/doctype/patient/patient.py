d2b8c6179d56f78728ed4331ec48b2767a84fd52
Gopi Kathiriya <gopikathiriya2003@gmail.com>
2023-10-05 14:41:17 +0530

    changes into the permission and hook file

--- exam_mode/exam_mode/doctype/patient/patient.py
+++ exam_mode/exam_mode/doctype/patient/patient.py
@@ -4,9 +4,14 @@
 
 import frappe
 from frappe.model.document import Document
-
+from frappe import throw
 class Patient(Document):
     def validate(self):
+        today = frappe.utils.today()
+        
+        if self.date_of_birth > today:
+            frappe.throw("Date of birth cannot be in the future.")
+        
         self.compute_age()
 
     def compute_age(self):
@@ -14,6 +19,9 @@ class Patient(Document):
             age_in_days = frappe.utils.date_diff(frappe.utils.today(), self.date_of_birth)
             self.age = age_in_days // 365
 
+    
+
+   
     def before_insert(doc, method=None):
         if doc.doctype == "Patient":
             patient_name = doc.patient_name
@@ -23,7 +31,10 @@ class Patient(Document):
                 "email": email,
                 "first_name": patient_name,
                 "new_password": "Sigzen@123#",
-                "user_type": "System User"
+                "user_type": "System User",
+                "role_profile_name":"patient role profile",
+                "module_profile":"exam_mode_profile"
             })
-            user.insert()
+            user.save(ignore_permissions=True)
             user.add_roles("Patient")
+            user.save(ignore_permissions=True)
