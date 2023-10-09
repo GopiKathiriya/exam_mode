1763e507dd1f05362562026aaa869abcff7e6e22
Gopi Kathiriya <gopikathiriya2003@gmail.com>
2023-10-09 12:47:45 +0530

    rename the report

---
+++ exam_mode/exam_mode/report/payment_detail/payment_detail.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, frappe and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Payment Detail"] = {
+	"filters": [
+		{
+			'fieldname': 'patient_name',
+			'label': 'Patient Name',
+			'fieldtype': 'Link',
+			'options': 'Patient'
+		},
+		{
+			'fieldname': 'payment_method',
+			'label': 'Payment Method',
+			'fieldtype': 'Select',
+			'options': ['By Cash','Online']
+		},
+		// {
+		// 	'fieldname': 'test_type',
+		// 	'label': 'Test Type',
+		// 	'fieldtype': 'Link',
+		// 	'options': 'Test Type'
+		// },
+
+	]
+};
+
