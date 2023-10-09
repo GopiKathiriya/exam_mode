1ab81d4c92308a20957ac4bc7a90f085f254dfb2
Gopi Kathiriya <gopikathiriya2003@gmail.com>
2023-10-09 12:18:58 +0530

    validate the custom button

--- exam_mode/exam_mode/doctype/exam_schedule/exam_schedule.js
+++ exam_mode/exam_mode/doctype/exam_schedule/exam_schedule.js
@@ -3,6 +3,7 @@ frappe.ui.form.on('Exam Schedule', {
         if (frm.doc.__islocal) {
             frm.page.clear_inner_toolbar();
         } else {
+            if (frappe.user.has_role('Doctor')) {
             frm.add_custom_button(__('Go To Examination'), function() {
                 var new_examination = frappe.model.get_new_doc("Examination");
                 new_examination.patient_name = frm.doc.patient_name;
@@ -12,4 +13,5 @@ frappe.ui.form.on('Exam Schedule', {
             });
         }
     }
+}
 });
